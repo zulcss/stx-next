0c3b7c7e5c38c91d925de6eb2d1e2d162c95835b
Charles Short <zulcss@gmail.com>
2022-10-22 23:34:58 +0000

    Enable docker
    Signed-off-by: Charles Short <zulcss@gmail.com>

--- README.rst
+++ README.rst
@@ -1,5 +1,8 @@
 What you will need on debian bullseye:
 
+Enable IP forwarding if not already enabled
+sysctl -w net.ipv4.ip_forward=1
+
 newer systemd:
 sudo apt-get install -y systemd/bullseye-backports
 
--- scripts/boot-container.sh
+++ scripts/boot-container.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo systemd-nspawn -b -i ../images/edgeos-core-0.1.img
+sudo systemd-nspawn --capability CAP_SYS_ADMIN,CAP_NET_ADMIN \
+	--system-call-filter="add_key keyctl bpf" \
+	--bind="/dev/fuse" \
+	--bind="/lib/modules/`uname -r`" \
+	-b -i ../images/edgeos-core-0.1.img
